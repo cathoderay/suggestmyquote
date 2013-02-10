2c9caa01e330f86c66a643bda6e1371780988297
Ronald Kaiser <raios.catodicos@gmail.com>
2013-02-10 12:27:57 -0200

    fixing analytics track id

--- media/js/behavior.js
+++ media/js/behavior.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
     //analytics
     var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-38146689-1']);
+    _gaq.push(['_setAccount', 'UA-38146689-2']);
     _gaq.push(['_trackPageview']);
 
     (function() {
@@ -23,5 +23,4 @@ $(document).ready(function() {
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
-
 });
