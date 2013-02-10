eda09c1c48c94e6b088f37336e78b2d3dcd7143a
Ronald Kaiser <raios.catodicos@gmail.com>
2013-02-10 14:22:47 -0200

    more quotes! =)

--- views.py
+++ views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render_to_response
 from django.views.decorators.cache import never_cache
 
+from quotes import quotes
 from core import get_quote
 
 
 @never_cache
 def home(request):
     quote, author = get_quote()
-    return render_to_response('index.html', {'quote': quote, 'author': author})
+    return render_to_response('index.html', {'quote': quote, 
+                                             'author': author, 
+                                             'number': len(quotes)})
 
